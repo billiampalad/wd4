9b639f5622652cb84065e6ff9f3ff4400041f810
bilclynten <billiamclynten@gmail.com>
2026-05-22 12:09:24 +0800

    add favicon tampilan humas

--- resources/views/auth/unit.blade.php
+++ resources/views/auth/unit.blade.php
@@ -5,7 +5,10 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <title>Unit — Sistem Informasi Kerjasama Polimdo</title>
+    <title>Sistem Informasi Kerjasama Politeknik Negeri Manado</title>
+
+    <link rel="icon" type="image/png" href="{{ asset('img/logo.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('img/logo.png') }}">
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
