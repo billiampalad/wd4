045a91b78d6176c4359abe29efd2e34f17e27246
bilclynten <billiamclynten@gmail.com>
2026-05-22 12:15:37 +0800

    add favicon tampilan ajukan dan pimpinan

--- resources/views/auth/pengajuan-mitra.blade.php
+++ resources/views/auth/pengajuan-mitra.blade.php
@@ -4,7 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ajukan Kerja Sama Mitra - Sistem Informasi Kerjasama</title>
+    <link rel="icon" type="image/png" href="{{ asset('img/logo.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('img/logo.png') }}">
+    <title>Ajukan Kerja Sama Mitra | Sistem Informasi Kerjasama Politeknik Negeri Manado</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
--- resources/views/auth/pimpinan.blade.php
+++ resources/views/auth/pimpinan.blade.php
@@ -4,8 +4,9 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-    <title>Pimpinan — Sistem Informasi Kerjasama Polimdo</title>
+    <link rel="icon" type="image/png" href="{{ asset('img/logo.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('img/logo.png') }}">
+    <title>Pimpinan | Sistem Informasi Kerjasama Politeknik Negeri Manado</title>
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
