834fd2985dd6a860bca68e25788068450a1bfa41
bilclynten <billiamclynten@gmail.com>
2026-09-24 22:34:33 +0800

    update error

--- app/Http/Controllers/Pimpinan/PengajuanKerjasamaMitraController.php
+++ app/Http/Controllers/Pimpinan/PengajuanKerjasamaMitraController.php
@@ -234,7 +234,7 @@ class PengajuanKerjasamaMitraController extends Controller
                         }
                     }
 
-                    /** @var \App\Models\Cooperation|null $cooperation */
+                    /** @var Cooperation|null $cooperation */
                     $cooperation = Cooperation::where('pengajuan_kerjasama_baru_id', $submission->id)->first();
                     if (! $cooperation) {
                         $cooperation = Cooperation::create([
